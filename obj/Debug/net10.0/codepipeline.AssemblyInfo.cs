3139d1db1ebab9af98172f07cd029de2a52403f9
Vamshi Krishna <myanavamshikrishna@gmail.com>
2026-04-05 18:27:10 +0530

    test

--- obj/Debug/net10.0/codepipeline.AssemblyInfo.cs
+++ obj/Debug/net10.0/codepipeline.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("codepipeline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5df6e525d85445c6489e65409805c15fd99d8ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("codepipeline")]
 [assembly: System.Reflection.AssemblyTitleAttribute("codepipeline")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
